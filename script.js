76bae611bdb9b67944391c3a74af787fc01fa6b7
Laine <103602883+cutielay@users.noreply.github.com>
2026-05-23 23:34:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -201,7 +201,7 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
 
-  const name = "JAQLIV";
+  const name = "LAINEBABY";
   let nameText = '';
   let nameIndex = 0;
   let isNameDeleting = false;
